c3010ec3044b1ae4af41c0c5cc4bbe464607f443
Shikizhan73 <112482196+Shikizhan73@users.noreply.github.com>
2023-06-25 21:44:41 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+import time
+
+def focus_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        min, sec = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(min, sec)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
